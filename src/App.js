56e70ca7c8314e5d4cf2a12f3d1741903e749ffc
anika121 <aarav.agarwal.000@gmail.com>
2021-03-01 13:58:43 -0500

    Jumbotron comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
 	return (
-		<p>Hello</p>
+		<JumbotronContainer />
 	);
 }
